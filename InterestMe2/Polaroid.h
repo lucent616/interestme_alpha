c8992a9f73e847827ee2d04e0d4a396eb8c4eb0b
lucent616 <collin@lifemademobile.com>
2014-07-15 16:00:47 -0700

    - Fully functional version of InterestMe with Backend integration
    Test
    - Trying to source content from Facebook social graph

--- InterestMe2/Polaroid.h
+++ InterestMe2/Polaroid.h
@@ -2,31 +2,60 @@
 //  Polaroid.h
 //  InterestMe2
 //
-//  Created by Collin Wallace on 6/2/14.
+//  Created by Collin Wallace on 7/10/14.
 //  Copyright (c) 2014 Collin Wallace. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
 #import <CoreData/CoreData.h>
 
+@class User;
 
 @interface Polaroid : NSManagedObject
 
 @property (nonatomic) BOOL boringToMe;
+@property (nonatomic, retain) NSString * genre;
+@property (nonatomic, retain) NSData * image;
 @property (nonatomic, retain) NSString * imageURL;
 @property (nonatomic) BOOL interestingToMe;
 @property (nonatomic) int32_t numberOfPeopleBoredByThis;
 @property (nonatomic) int32_t numberOfPeopleInterestedInThis;
 @property (nonatomic) int32_t numberOfTimesSaved;
 @property (nonatomic) int32_t numberOfTimesSent;
+@property (nonatomic) int64_t polaroid_ID;
+@property (nonatomic, retain) NSString * polaroidDescription;
 @property (nonatomic) BOOL savedByMe;
 @property (nonatomic) BOOL sentByMe;
 @property (nonatomic, retain) NSString * title;
-@property (nonatomic, retain) NSString * genre;
-@property (nonatomic, retain) NSData * image;
-@property (nonatomic, retain) NSManagedObject *wasBoringToThisUser;
-@property (nonatomic, retain) NSManagedObject *wasInterestingToThisUser;
-@property (nonatomic, retain) NSManagedObject *wasSavedByThisUser;
-@property (nonatomic, retain) NSManagedObject *wasSentByThisUser;
+@property (nonatomic, retain) NSString * sourceURL;
+@property (nonatomic, retain) NSString * author;
+@property (nonatomic) NSTimeInterval dateTaken;
+@property (nonatomic, retain) NSSet *allUsersBoredByThisPolaroid;
+@property (nonatomic, retain) NSSet *allUsersInterestedByThisPolaroid;
+@property (nonatomic, retain) NSSet *allUsersThatSavedThisPolaroid;
+@property (nonatomic, retain) NSSet *allUsersThatSentThisPolaroid;
+@end
+
+@interface Polaroid (CoreDataGeneratedAccessors)
+
+- (void)addAllUsersBoredByThisPolaroidObject:(User *)value;
+- (void)removeAllUsersBoredByThisPolaroidObject:(User *)value;
+- (void)addAllUsersBoredByThisPolaroid:(NSSet *)values;
+- (void)removeAllUsersBoredByThisPolaroid:(NSSet *)values;
+
+- (void)addAllUsersInterestedByThisPolaroidObject:(User *)value;
+- (void)removeAllUsersInterestedByThisPolaroidObject:(User *)value;
+- (void)addAllUsersInterestedByThisPolaroid:(NSSet *)values;
+- (void)removeAllUsersInterestedByThisPolaroid:(NSSet *)values;
+
+- (void)addAllUsersThatSavedThisPolaroidObject:(User *)value;
+- (void)removeAllUsersThatSavedThisPolaroidObject:(User *)value;
+- (void)addAllUsersThatSavedThisPolaroid:(NSSet *)values;
+- (void)removeAllUsersThatSavedThisPolaroid:(NSSet *)values;
+
+- (void)addAllUsersThatSentThisPolaroidObject:(User *)value;
+- (void)removeAllUsersThatSentThisPolaroidObject:(User *)value;
+- (void)addAllUsersThatSentThisPolaroid:(NSSet *)values;
+- (void)removeAllUsersThatSentThisPolaroid:(NSSet *)values;
 
 @end
--- InterestMe2/Polaroid.m
+++ InterestMe2/Polaroid.m
@@ -2,30 +2,36 @@
 //  Polaroid.m
 //  InterestMe2
 //
-//  Created by Collin Wallace on 6/2/14.
+//  Created by Collin Wallace on 7/10/14.
 //  Copyright (c) 2014 Collin Wallace. All rights reserved.
 //
 
 #import "Polaroid.h"
+#import "User.h"
 
 
 @implementation Polaroid
 
 @dynamic boringToMe;
+@dynamic genre;
+@dynamic image;
 @dynamic imageURL;
 @dynamic interestingToMe;
 @dynamic numberOfPeopleBoredByThis;
 @dynamic numberOfPeopleInterestedInThis;
 @dynamic numberOfTimesSaved;
 @dynamic numberOfTimesSent;
+@dynamic polaroid_ID;
+@dynamic polaroidDescription;
 @dynamic savedByMe;
 @dynamic sentByMe;
 @dynamic title;
-@dynamic genre;
-@dynamic image;
-@dynamic wasBoringToThisUser;
-@dynamic wasInterestingToThisUser;
-@dynamic wasSavedByThisUser;
-@dynamic wasSentByThisUser;
+@dynamic sourceURL;
+@dynamic author;
+@dynamic dateTaken;
+@dynamic allUsersBoredByThisPolaroid;
+@dynamic allUsersInterestedByThisPolaroid;
+@dynamic allUsersThatSavedThisPolaroid;
+@dynamic allUsersThatSentThisPolaroid;
 
 @end
