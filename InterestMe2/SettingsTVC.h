f45f6dbba5e8565f424642fe1ed10e7864fb931e
lucent616 <collin@lifemademobile.com>
2014-07-15 16:14:29 -0700

    Milestone commit - Fully functional

---
+++ InterestMe2/SettingsTVC.h
@@ -0,0 +1,13 @@
+//
+//  SettingsTVC.h
+//  InterestMe2
+//
+//  Created by Collin Wallace on 7/15/14.
+//  Copyright (c) 2014 Collin Wallace. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingsTVC : UITableViewCell
+
+@end
